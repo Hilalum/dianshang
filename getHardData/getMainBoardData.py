0ee28b780c9323efd7d22a686e3ae6746e2ae947
Zhgy <742722680@qq.com>
2018-06-22 17:58:58 +0800

    完成了主板信息爬虫

--- getHardData/getMainBoardData.py
+++ getHardData/getMainBoardData.py
@@ -1,4 +1,8 @@
-# 主板信息爬虫
+# ------------------------------------------------------------
+# ------------------------------------------------------------
+# --------------------   主板信息爬虫   ----------------------
+# ------------------------------------------------------------
+# ------------------------------------------------------------
 
 import urllib.request
 from bs4 import BeautifulSoup
@@ -33,13 +37,47 @@ url_dict = {
 }
 
 for key in url_dict:
-    print(key, "的系列有：")
     req = urllib.request.Request(url_dict[key])
     r = urllib.request.urlopen(req)
     html = r.read().decode('gbk')
     soup = BeautifulSoup(html, 'html.parser')
     all_a = soup.find(id='J_SeriesFilter').find_all('div')[0].find_all('div')[0].find_all('a')
     for item in all_a:
-        # 品牌为key
-        name = item.get_text()  # 系列名称
-        item_url = "http://detail.zol.com.cn/series/5/"+item.get('href').split('/')[-1]
+        name = item.get_text()
+        seriesId = item.get('href').split('/')[-1].split('.')[0].split('_')[0]
+        page = 1
+        while True:
+            url = "http://detail.zol.com.cn/xhr4_Series_GetSeriesPro_isAjax=1%5EseriesId="+str(seriesId)+"%5EmainId=1210881%5EsubcateId=5%5Epage="+str(page)+"%5EparamStr=.html"
+            page = page + 1
+            req = urllib.request.Request(url)
+            r = urllib.request.urlopen(req)
+            html = r.read().decode('utf-8')
+            if len(html)==60:
+                break;
+            soup = BeautifulSoup(html, 'html.parser')
+            for i in soup.find_all('a'):
+                try:
+                    idname = i['title']
+                    url = "http://detail.zol.com.cn/motherboard/"+i['href'].split('/')[-1]
+                    req = urllib.request.Request(url)
+                    r = urllib.request.urlopen(req)
+                    html = r.read().decode('gbk', errors='ignore')
+                    soup = BeautifulSoup(html, 'html.parser')
+                    price = soup.select('.price-type')[0].get_text()
+                    all_li = soup.select('.section-content')[0].find('ul').find_all('li')
+                    # 获取数据
+                    print(key)  # 品牌名
+                    print(name)  # 系列名
+                    print(idname)  # 产品型号名
+                    print(price)  # 价格
+                    print(all_li[0].find_all('p')[0].get_text().split("：")[1])  # 主芯片组
+                    print(all_li[0].find_all('p')[1].get_text().split("：")[1])  # 音频芯片
+                    print(all_li[1].find_all('p')[0].get_text().split("：")[1])  # 内存类型
+                    print(all_li[1].find_all('p')[1].get_text().split("：")[1])  # 最大内存容量
+                    print(all_li[2].find_all('p')[0].get_text().split("：")[1])  # 主板版型
+                    print(all_li[2].find_all('p')[1].get_text().split("：")[1])  # 外形尺寸
+                    print(all_li[3].find_all('p')[0].get_text().split("：")[1])  # 电源插口
+                    print(all_li[3].find_all('p')[1].get_text().split("：")[1])  # 供电模式
+                    print()
+                except Exception as e:
+                    pass
